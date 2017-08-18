ff10bef813827ba98e28831d07a7c80d495e4a08
Marshall Lerner <marshall_lerner@brown.edu>
2017-08-18 16:51:59 -0500

    More of the webpage is coming along

--- test/test.py
+++ test/test.py
@@ -1,19 +1,47 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request, make_response
+from flask import flash, session
+import os
+
 app = Flask('test')
+app.secret_key = os.urandom(12)
 
 @app.route("/")
 def index():
-  return render_template('home.html')
+  if not session.get('logged_in'):
+    return render_template('login.html')
+  else:
+    return display_user(session.get('loginName'))
 
-@app.route('/login')
+@app.route('/login', methods=['POST'])
 def login():
-  return render_template('login.html')
+  if valid_login(request.form['username'],
+                 request.form['password']):
+    session['logged_in'] = True
+    session['loginName'] = request.form['username']
+    return index()
+    #log_the_user_in(request.form['username'])
+  else:
+    flash('These credentials were not found. Please try again')
+    return render_template('login.html')
 
-@app.route('/user/<username>')
-def profile(username): pass
+def valid_login(usr, psw):
+    # TODO
+    #set_cookie("loginActive", value=usr,max_age=14400)
+    return True
 
-with app.test_request_context():
-  url_for('index')
-  url_for('login')
-  url_for('login', chuck='hi')
-  url_for('profile', username='Marshall Lerner')
+@app.route('/user')
+def display_user(username):
+   return "Hello World"
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return index()
+
+  #return "Hello World"
+
+#with app.test_request_context():
+  #url_for('index')
+  #url_for('login')
+ # url_for('login', chuck='hi')
+#  url_for('profile', username='Marshall Lerner')
